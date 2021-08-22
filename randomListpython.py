0b3a5502b1040356e3ecee7b6db9043de3190774
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-08-22 10:53:59 +0000

    Add files via upload

---
+++ randomListpython.py
@@ -0,0 +1,8 @@
+import random
+def generaterandomlist():
+                        #10 is the lowest fucking number
+                            #30 is the highest
+                            # 5 is the amount of numbers 
+    print(random.sample(range(10,30),5))
+
+generaterandomlist()
