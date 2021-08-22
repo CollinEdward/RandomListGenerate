b0a65c22892e19abde8c78b55df95e18a6428c6a
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-08-22 10:56:07 +0000

    Fix

--- randomListpython.py
+++ randomListpython.py
@@ -1,6 +1,6 @@
-import random
-def generaterandomlist():
-                        #10 is the lowest fucking number
+import random # random is a python moduel
+def generaterandomlist(): # function
+                        # 10 is the lowest number
                             #30 is the highest
                             # 5 is the amount of numbers 
     print(random.sample(range(10,30),5))
